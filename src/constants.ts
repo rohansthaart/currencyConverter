fe595c9aee911b2610c65e1bf2df871e1081b283
rohansthaart <rohan-stha@live.com>
2024-05-25 11:33:41 +0545

    converter

---
+++ src/constants.ts
@@ -0,0 +1,106 @@
+/* eslint-disable prettier/prettier */
+export const currencyByRupee = [
+  {
+    name: 'DOLLAR',
+    value: 0.00752,
+    flag: '🇺🇸',
+    symbol: '$',
+  },
+  {
+    name: 'EURO',
+    value: 0.0069,
+    flag: '🇪🇺',
+    symbol: '€',
+  },
+  {
+    name: 'POUND',
+    value: 0.0059,
+    flag: '🇬🇧',
+    symbol: '£',
+  },
+  {
+    name: 'RUBEL',
+    value: 0.674,
+    flag: '🇷🇺',
+    symbol: '₽',
+  },
+  {
+    name: 'AUS DOLLAR',
+    value: 0.0115,
+    flag: '🇦🇺',
+    symbol: 'A$',
+  },
+  {
+    name: 'CAN DOLLAR',
+    value: 0.0103,
+    flag: '🇨🇦',
+    symbol: 'C$',
+  },
+  {
+    name: 'YEN',
+    value: 1.18,
+    flag: '🇯🇵',
+    symbol: '¥',
+  },
+  {
+    name: 'DINAR',
+    value: 0.0023,
+    flag: '🇰🇼',
+    symbol: 'KD',
+  },
+  {
+    name: 'BITCOIN',
+    value: 0.000000254,
+    flag: '🎰',
+    symbol: '₿',
+  },
+  {
+    name: 'INDIAN RUPEE',
+    value: 0.625,
+    flag: '🇮🇳',
+    symbol: '₹',
+  },
+  {
+    name: 'CHINESE YUAN',
+    value: 0.053,
+    flag: '🇨🇳',
+    symbol: '¥',
+  },
+  {
+    name: 'SINGAPORE DOLLAR',
+    value: 0.0102,
+    flag: '🇸🇬',
+    symbol: 'S$',
+  },
+  {
+    name: 'SWISS FRANC',
+    value: 0.0069,
+    flag: '🇨🇭',
+    symbol: 'CHF',
+  },
+  {
+    name: 'UAE DIRHAM',
+    value: 0.0276,
+    flag: '🇦🇪',
+    symbol: 'د.إ',
+  },
+  {
+    name: 'MALAYSIAN RINGGIT',
+    value: 0.0354,
+    flag: '🇲🇾',
+    symbol: 'RM',
+  },
+  {
+    name: 'SAUDI RIYAL',
+    value: 0.0282,
+    flag: '🇸🇦',
+    symbol: '﷼',
+  },
+  {
+    name: 'SOUTH AFRICAN RAND',
+    value: 0.138,
+    flag: '🇿🇦',
+    symbol: 'R',
+  },
+];
+
